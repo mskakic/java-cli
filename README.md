5a66c80b558d119abbf9e9b3fce70c0722181c8f
mskakic <74156761+mskakic@users.noreply.github.com>
2024-04-20 18:03:44 +0200

    Menu

--- README.md
+++
@@ -1,2 +0,0 @@
-# java-cli
-Java - CLI
---
+++ src/main/java/App.java
@@ -0,0 +1,43 @@
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.Scanner;
+
+@Command(name = "java-cli", mixinStandardHelpOptions = true, version = "1.0",
+        description = "CLI Application with Picocli")
+public class App implements Runnable {
+
+    @Override
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            System.out.println("Menu:");
+            System.out.println("1. Logon na OCP");
+            System.out.println("2. Quit");
+
+            System.out.print("Izbor: ");
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    sendHttpRequest();
+                    break;
+                case "2":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Krivi odabir");
+            }
+        }
+    }
+
+    private void sendHttpRequest() {
+        // Your HTTP request code goes here
+        System.out.println("Sending HTTP request i response za Logon...");
+    }
+
+    public static void main(String[] args) {
+        CommandLine.run(new App(), args);
+    }
+}
